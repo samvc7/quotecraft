f13d53e5fd6889db8afc55d9ea119f558ae5d7fa
Vicente Samuel Cala <vicente.cala07@gmail.com>
2024-12-10 12:00:52 +0100

    chore: Replaced next js logo with own logo

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,9 @@ export default async function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
+          src="/cala7.png"
+          alt="Cala7 Logo"
+          width={80}
           height={37}
           priority
         />
