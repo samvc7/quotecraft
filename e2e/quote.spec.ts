42cff57644da114dc15d9fe0c9aec3e8b58d7851
Vicente Samuel Cala <vicente.cala07@gmail.com>
2024-12-11 21:44:17 +0100

    test: Added filtering and next quote tests

--- e2e/quote.spec.ts
+++ e2e/quote.spec.ts
@@ -1,11 +1,13 @@
 import { test, expect } from "@playwright/test";
 
-// TODO: implement
-test("initial quote", async ({ page }) => {
+test("Initial quote", async ({ page }) => {
   await page.goto("http://localhost:3000");
+  const quote = await page.getByRole("blockquote").textContent();
+  expect(quote).not.toBeNull();
+  expect(quote).not.toBe("");
 });
 
-test("Filter quotes", async ({ page }) => {
+test("Filter quotes visible", async ({ page }) => {
   await page.goto("http://localhost:3000");
   await expect(page.getByRole("button", { name: "Select Tags" })).toBeVisible();
   await expect(
@@ -14,4 +16,56 @@ test("Filter quotes", async ({ page }) => {
 
   // When manually adding name to button component, locator is needed here rather than getByRole
   await expect(page.locator('button[name="Next Quote"]')).toBeVisible();
+});
+
+test("Next quote", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  for (let i = 0; i < 10; i++) {
+    const quoteBefore = await page.getByRole("blockquote").textContent();
+    await page.locator('button[name="Next Quote"]').click();
+    const quoteAfter = await page.getByRole("blockquote").textContent();
+
+    expect(quoteBefore).not.toEqual(quoteAfter);
+  }
+});
+
+// TODO: test in combination with next quote button
+test("Filter by tags", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  expect(page.getByRole("button", { name: "Life Love" })).not.toBeVisible();
+
+  await page.getByRole("button", { name: "Select Tags" }).click();
+  await page.getByRole("option", { name: "Life" }).click();
+  await page.getByRole("option", { name: "Love" }).click();
+  await page.getByRole("blockquote").click();
+
+  await expect(page.getByRole("button", { name: "Life Love" })).toBeVisible();
+});
+
+test("Filter by authors", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  expect(page.getByRole("button", { name: "Shakespeare" })).not.toBeVisible();
+
+  await page.getByRole("button", { name: "Select Authors" }).click();
+  await page.getByRole("option", { name: "Shakespeare" }).click();
+  await page.getByRole("blockquote").click({ position: { x: 0, y: 0 } });
+
+  await expect(page.getByRole("button", { name: "Shakespeare" })).toBeVisible();
+
+  await expect(page.getByRole("blockquote")).toContainText("Shakespeare");
+});
+
+test("Filter not found", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  await page.getByRole("button", { name: "Select Tags" }).click();
+  await page.getByRole("option", { name: "Life" }).click();
+  await page.getByRole("option", { name: "Love" }).click();
+  await page.getByRole("option", { name: "Change" }).click();
+  await page.getByRole("blockquote").click();
+
+  await expect(page.getByRole("blockquote")).toContainText("No quotes found");
 });
